d9aaaf08e0147f9c4cfb14919cb1921a7ff01fe6
Unknown <fishandcat@naver.com>
2018-06-28 17:05:19 +0900

    프레그먼트 베이스
    교육과정 통합

--- app/src/main/java/com/algorithm416/csjolup/MainActivity.java
+++ app/src/main/java/com/algorithm416/csjolup/MainActivity.java
@@ -1,13 +1,39 @@
 package com.algorithm416.csjolup;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.time.Instant;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ConstraintLayout select;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    select = (ConstraintLayout) findViewById(R.id.select);
     }
+
+
+    public void onClick (View v) {
+
+        switch (v.getId()) {
+            case R.id.savebtn:
+                select.setVisibility(View.GONE);
+                FragmentManager curriculumFragmentManager = getSupportFragmentManager();
+                FragmentTransaction curriculumFragmentTransaction = curriculumFragmentManager.beginTransaction();
+                curriculumFragmentTransaction.replace(R.id.fragment, curriculum.newInstance("ttt1", "ttt2"));
+                curriculumFragmentTransaction.commit();
+                break;
+
+        }
+    }
+
 }
